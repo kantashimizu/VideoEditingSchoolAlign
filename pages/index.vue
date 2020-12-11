0db897ba399875ffbd145efe34158880561da208
比嘉盛人 <twt710hs@gmail.com>
2020-12-11 22:05:31 +0900

    TOP:pug作成

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,129 @@
 <template lang="pug">
 section.home
-  h1 トップページ
-    p コーディングお願い致します！
+  h1 VideoEdithingSchoolAlign
+    // p コーディングお願い致します！
+    ul
+      li 口コミを書く
+      li お問い合わせ
+    ul
+      li HOME
+      li ランキング
+      li カテゴリ
+    ul
+      li アイテム
+      li HOME
+      li ランキング
+      li カテゴリ
+      li HOME
+      li ランキング
+      li カテゴリ
+      li HOME
+      li ランキング
+      li カテゴリ
+      li カテゴリ
+    ul
+      li スクール検索
+    ul
+      li カテゴリ
+      li HOME
+      li ランキング
+      li カテゴリ
+      li カテゴリ
+    ul
+      li  口コミを書く
+    p MENU
+  h2 検索
+
+  h2 カテゴリ
+
+  h2 受講場所
+
+  h2 価格
+
+  h2 口コミ
+
+  h2 詳細条件
+    ul
+      li オンライン
+      li 無料体験
+      li 就職サポート
+
+    ul
+      li トップページ
+  h2 おすすめランキング
+
+    div もっとランキングを見る
+
+    div
+  h2 カテゴリ
+  div
+    a(href="/") Webアプリ開発
+    a(href="/") スマホアプリ開発
+    a(href="1") UX/UIデザイン
+    a(href="1") ネットワーク・インフラ AI（人工知能）
+    a(href="1") IoT・ロボット ゲーム（AR・VR）
+    a(href="1") ブロックチェーン データサイエンス
+    a(href="1") サイバーセキュリティ
+  h2 受講場所
+    ul
+      li (地域名)
+      li (地域名)
+      li (地域名)
+      li (地域名)
+      li (地域名)
+      li (地域名)
+      li (地域名)
+      li (地域名)
+      li (地域名)
+      li (地域名)
+      li (地域名)
+      li (地域名)
+      li (地域名)
+      li (地域名)
+      li (地域名)
+      li (地域名)
+      li (地域名)
+      li (地域名)
+      li (地域名)
+      li (地域名)
+  h2 オンラインスクール
+    p 画像挿入
+  h2 RANKING
+  h2 ランキング
+    ul
+      li: a(href="/") １
+      li: a(href="/") ２
+      li: a(href="/") ３
+      li: a(href="/") ４
+      li: a(href="/") ５
+  // footer
+  ul
+    li アイテム
+    li HOME
+    li ランキング
+    li カテゴリ
+    li HOME
+  ul
+    li アイテム
+    li HOME
+    li ランキング
+  h2 お問い合わせ
+  p 当サイトに関するお問い合わせフォームです。<br>
+    | &emsp;お問い合わせフォーム
+  p VideoEdithingSchoolAlign
+  p プログラミングスクール口コミ・評判サイト VideoEdithingSchoolAlign
+  ul
+    li: a(href="/") SNS
+    li: a(href="/") SNS
+    li: a(href="/") SNS
+    li: a(href="/") SNS
+    li: a(href="/") SNS
+  p © 2020 VideoEdithingSchoolAlign
+
+
+
+
+
 
 </template>
 
@@ -14,6 +136,7 @@ export default {}
   background: yelloow
   h1
     color: red
-    p
-      font-size: 10px
+    ul
+      margin: 20px;
+
 </style>
