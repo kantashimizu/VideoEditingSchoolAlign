e6ba942140c3c1ebd9c4e4f0d7ef9e98ef9c0be1
kantashimizu <gantaiqingshui968@gmail.com>
2020-12-21 12:25:49 +0900

    add:セレクトボックス

--- pages/index.vue
+++ pages/index.vue
@@ -8,29 +8,44 @@ section.home
         p.btn_left 口コミを書く
         p.btn_right お問い合わせ
     div.header_bottom
-      div: img(src="https://placehold.jp/350x60.png")
+      img(src="https://placehold.jp/350x60.png")
       ul.links
         li HOME
         li ランキング
         li カテゴリ
         li スクール検索
         li 口コミを書く
+  div.search
     div.search_home
-      div.search_home-centerbox
-        div.search_home-text
-          h1 検索
-            ul
-              li.select_box 価格
-              li.select_box 詳細条件
-              li.select_box 口コミ
-              li.select_box 受講場所
-          h1 詳細条件
-    div.main-content
-      div.main-ranking
-        h1.main-ranking おすすめランキング
-
-    div もっとランキングを見る
-
+      div.search_home-input
+        h2 検索
+        input(
+          placeholder="検索"
+          name="search_text"
+          type="text"
+        )
+      div.search_home-selects
+        div.select
+          p 受講場所
+          v-select.input(
+            v-model="selected"
+            placeholder="価格を選択"
+            :options="price"
+          )
+        div.select
+          p 価格
+          v-select.input(
+            v-model="selected"
+            placeholder="価格を選択"
+            :options="price"
+          )
+        div.select
+          p 口コミ
+          v-select.input(
+            v-model="selected"
+            placeholder="価格を選択"
+            :options="price"
+          )
     div
   h2 カテゴリ
   div
@@ -99,12 +114,29 @@ section.home
 </template>
 
 <script>
-export default {}
+export default {
+  name: 'index',
+  data() {
+    return {
+      selected: '',
+      prefecture: {},
+      price: ['無料','5万円未満','5万円〜10万円','10万円〜20万円','20万円〜50万円','50万円以上']
+    }
+  }
+}
 </script>
 <style lang="sass">
 body
   padding: 0
   margin: 0
+p
+  margin: 0
+h2
+  margin: 0
+.vs__search, .vs__search:focus
+  display: none
+.vs__clear
+  display: none
 </style>
 
 
@@ -153,26 +185,59 @@ body
         width: auto
         li
           margin-left: 22px
+.search
+  background: red
+  padding: 10px 0
   .search_home
-    padding: 15px 50px
-    margin: 0 0
-    height: 350px
-    background: #f9cc46
-    .search_home-centerbox
-      align-items: center
-      justify-content: space-between
-      border-radius: 20px
-      background: #ffffff
-      height: 100%
-      float: left
-      width: 100%
-      .search_home-text
-        display: inline-block
-        .select_box
-          display: inline-block
-          list-style: none
-          margin: 23px 89px
-    .main-content
+    max-width: 900px
+    background: #ffffff
+    border-radius: 5px
+    margin: 0 auto
+    padding: 30px 5% 15px 5%
+    &-input
+      h2
+        font-size: 20px
+        color: #1a3153
+      input
+        width: 100%
+        padding: 0 0 0 8px
+        color: #1a3153
+        height: 36px
+        border: 1px solid #dddddd
+        border-radius: 2px
+    &-selects
+      display: flex
+      color: #1a3153
+      .select
+        margin-top: 18px
+        width: 33%
+        p
+          font-size: 16px
+          font-weight: bold
+        .input
+          border: 1px solid #1a3153
+          border-radius: 5px
+          width: 70%
+          span
+            white-space: nowrap
+            font-size: 12px
+        #vs1__combobox
+          border: none
+    // .search_home-centerbox
+    //   align-items: center
+    //   justify-content: space-between
+    //   border-radius: 20px
+    //   background: #ffffff
+    //   height: 100%
+    //   float: left
+    //   width: 100%
+    //   .search_home-text
+    //     display: inline-block
+    //     .select_box
+    //       display: inline-block
+    //       list-style: none
+    //       margin: 23px 89px
+    // .main-content
       //padding: 30px 5%
       //width: 100%
       //max-width: 100%
